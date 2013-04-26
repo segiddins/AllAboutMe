e05e069bcea8934916f0db8aa56e92189f57bff0
Samuel E. Giddins <segiddins@segiddins.me>
2013-04-25 21:19:17 -0400

    Initial Commit

---
+++ AllAboutMe/SEGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SEGAppDelegate.h
+//  AllAboutMe
+//
+//  Created by Samuel E. Giddins on 4/25/13.
+//  Copyright (c) 2013 Samuel E. Giddins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SEGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AllAboutMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AllAboutMe
+//
+//  Created by Samuel E. Giddins on 4/25/13.
+//  Copyright (c) 2013 Samuel E. Giddins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SEGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SEGAppDelegate class]));
+    }
+}
